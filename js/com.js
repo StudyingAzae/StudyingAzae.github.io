b70ceb8cf570bd4acc9c1c3b4dd82df0d4bad441
StudyingAzae <86333914+StudyingAzae@users.noreply.github.com>
2022-02-24 00:28:25 +0900

    Update com.js

--- js/com.js
+++ js/com.js
@@ -1,9 +1,17 @@
 // null check
 function isNull(str){
   if(str == null || str == "" || str == "undefined"){
+    return true; 
+  }
+  
+  return false;
+}
+
+// not null check
+function isNotNull(str){
+  if(str == null || str == "" || str == "undefined"){
     return false; 
   }
   
   return true;
 }
-
